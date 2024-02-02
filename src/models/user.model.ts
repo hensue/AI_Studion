7a845c39f5ec83d31f6a507fbf6a74b6d01b5d42
hensue <138180419+hensue@users.noreply.github.com>
2024-02-02 04:22:04 -0800

    Update user.model.ts

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose"
-import bcrypt from "bcrypt"
+// import bcrypt from "bcrypt"
 
 mongoose.connect("mongodb+srv://StartupSphere:Freelancer123-@startupsphere.gr5qamc.mongodb.net");
 
